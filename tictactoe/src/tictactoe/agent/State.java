af6f762535e5afa6a8fd615ed4ef0d2a47a6752d
docplasma <thedocplasma@gmail.com>
2014-03-24 13:50:29 -0500

    Updated State
    Move functions now require which player is moving
    State now has a value which tells if the state is valid or not
    Start and end code is now init

--- tictactoe/src/tictactoe/agent/State.java
+++ tictactoe/src/tictactoe/agent/State.java
@@ -5,6 +5,7 @@ import java.util.Vector;
 
 public class State {
 
+	boolean validState;
 	int numWhitePieces;
 	int numBlackPieces;
 	Vector<Integer> tiles;
@@ -16,6 +17,7 @@ public class State {
 	}
 	
 	private void initState() {
+		validState = true;
 		numWhitePieces = 20;
 		numBlackPieces = 20;
 		tiles = new Vector<Integer>(32);
@@ -30,8 +32,10 @@ public class State {
 		
 	}
 	public boolean isValid() {
-		// TODO Auto-generated method stub
-		return false;
+		return validState;
+	}
+	public void setValid(boolean val) {
+		validState = val;
 	}
 	public float getHVal() {
 		// TODO Auto-generated method stub
@@ -41,33 +45,45 @@ public class State {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	public static Vector<State> getChildren(State state) {
+	public static Vector<State> getChildren(State state, int player) {
 		Vector<State> childStates = new Vector<State>();
-		childStates.addAll(moveNW(state, childStates));
-		childStates.addAll(moveNE(state, childStates));
-		childStates.addAll(moveSW(state, childStates));
-		childStates.addAll(moveSE(state, childStates));
-		childStates.addAll(jump(state, childStates));
+		childStates.addAll(moveNW(state, childStates, player));
+		childStates.addAll(moveNE(state, childStates, player));
+		childStates.addAll(moveSW(state, childStates, player));
+		childStates.addAll(moveSE(state, childStates, player));
+		childStates.addAll(jump(state, childStates, player));
 		return childStates;
 	}
-	private static Vector<State> moveNW(State state, Vector<State> childStates) {
+	private static Vector<State> moveNW(State state, Vector<State> childStates, int player) {	
+		Vector<Integer> tiles = state.getTiles();
 		//Init start/end to moving sides pieces
-		int start;
-		int end;
-		//TODO Determine way to know whos children we are generating
-		//If player 1 children
-		if(true) {
-			start = 0;
-			end = 0;
-		} else { //If player 2 children
-			start = 0;
-			end = 0;
-		}
+		int start = 0;
+		int end = tiles.size() - 1;
+		//TODO Determine way to know whos children we are generating?
 		
 		//Generate states for all pieces of current side
 		while(start++<end) {
+			//Skip blank spaces and pieces not on current side
+			if(tiles.get(start) == 0 ||
+					tiles.get(start) % 2 == 0 && player == 1 ||
+					tiles.get(start) % 2 == 1 && player == 0) {
+				continue;
+			}
 			State possibleState = new State(state);
-			swap(possibleState.getTiles(), start-5, start); //TODO get correct offset value
+			if(start%4%2 == 0) {
+				if (start - 4 < 0) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start-4, start);
+				}
+				
+			} else {
+				if (start - 5 < 0) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start-5, start);
+				}
+			}
 			//If the move was actually possible, add to list of states
 			if(possibleState.isValid()) {
 				childStates.add(possibleState);
@@ -75,20 +91,150 @@ public class State {
 		}
 		return childStates;
 	}
-	private static Vector<State> moveNE(State state, Vector<State> childStates) {
-		// TODO Auto-generated method stub
+	private static Vector<State> moveNE(State state, Vector<State> childStates, int player) {
+		Vector<Integer> tiles = state.getTiles();
+		//Init start/end to moving sides pieces
+		int start = 0;
+		int end = tiles.size() - 1;
+		//TODO Determine way to know whos children we are generating
+
+		//Generate states for all pieces of current side
+		while(start++<end) {
+			//Skip blank spaces and pieces not on current side
+			if(tiles.get(start) == 0 ||
+					tiles.get(start) % 2 == 0 && player == 1 ||
+					tiles.get(start) % 2 == 1 && player == 0) {
+				continue;
+			}
+			State possibleState = new State(state);
+			if(start%4%2 == 0) {
+				if (start - 3 < 0) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start - 3, start);
+				}
+				
+			} else {
+				if (start - 4 < 0) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start - 4, start);
+				}
+			}
+			//If the move was actually possible, add to list of states
+			if(possibleState.isValid()) {
+				childStates.add(possibleState);
+			}
+		}
 		return childStates;
 	}
-	private static Vector<State> moveSW(State state, Vector<State> childStates) {
-		// TODO Auto-generated method stub
+	private static Vector<State> moveSW(State state, Vector<State> childStates, int player) {
+		Vector<Integer> tiles = state.getTiles();
+		//Init start/end to moving sides pieces
+		int start = 0;
+		int end = tiles.size() - 1;
+		//TODO Determine way to know whos children we are generating
+
+		//Generate states for all pieces of current side
+		while(start++<end) {
+			//Skip blank spaces and pieces not on current side
+			if(tiles.get(start) == 0 ||
+					tiles.get(start) % 2 == 0 && player == 1 ||
+					tiles.get(start) % 2 == 1 && player == 0) {
+				continue;
+			}
+			State possibleState = new State(state);
+			if(start%4%2 == 0) {
+				if (start + 4 > tiles.size() - 1) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start + 4, start);
+				}
+				
+			} else {
+				if (start + 3 > tiles.size() - 1) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start + 3, start);
+				}
+			}
+			//If the move was actually possible, add to list of states
+			if(possibleState.isValid()) {
+				childStates.add(possibleState);
+			}
+		}
 		return childStates;
 	}
-	private static Vector<State> moveSE(State state, Vector<State> childStates) {
-		// TODO Auto-generated method stub
+	private static Vector<State> moveSE(State state, Vector<State> childStates, int player) {
+		Vector<Integer> tiles = state.getTiles();
+		//Init start/end to moving sides pieces
+		int start = 0;
+		int end = tiles.size() - 1;
+
+		//Generate states for all pieces of current side
+		while(start++<end) {
+			//Skip blank spaces and pieces not on current side
+			if(tiles.get(start) == 0 ||
+					tiles.get(start) % 2 == 0 && player == 1 ||
+					tiles.get(start) % 2 == 1 && player == 0) {
+				continue;
+			}
+			State possibleState = new State(state);
+			if(start%4%2 == 0) {
+				if (start + 5 > tiles.size() - 1) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start + 5, start);
+				}
+				
+			} else {
+				if (start + 4 > tiles.size() - 1) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start + 4, start);
+				}
+			}
+			//If the move was actually possible, add to list of states
+			if(possibleState.isValid()) {
+				childStates.add(possibleState);
+			}
+		}
 		return childStates;
 	}
-	private static Vector<State> jump(State state, Vector<State> childStates) {
-		// TODO Auto-generated method stub
+	private static Vector<State> jump(State state, Vector<State> childStates, int player) {
+		Vector<Integer> tiles = state.getTiles();
+		//Init start/end to moving sides pieces
+		int start = 0;
+		int end = tiles.size() - 1;
+		
+		//Generate states for all pieces of current side
+		while(start++<end) {
+			//Skip blank spaces and pieces not on current side
+			if(tiles.get(start) == 0 ||
+					tiles.get(start) % 2 == 0 && player == 1 ||
+					tiles.get(start) % 2 == 1 && player == 0) {
+				continue;
+			}
+			State possibleState = new State(state);
+			if(start%4%2 == 0) {
+				if (start - 4 < 0) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start-4, start);
+				}
+				
+			} else {
+				if (start - 5 < 0) {
+					possibleState.isValid();
+				} else {
+					swap(tiles, start-5, start);
+				}
+			}
+			//If the move was actually possible, add to list of states
+			if(possibleState.isValid()) {
+				childStates.add(possibleState);
+			}
+		}
 		return childStates;
 	}
 	private static void swap(Vector<Integer> tiles, int a, int b) {
